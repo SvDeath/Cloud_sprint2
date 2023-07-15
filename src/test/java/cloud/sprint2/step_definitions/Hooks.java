7ea8ee5910b2cd4cbcc6e2192c9aa39798696135
Roman <romanaliev808@gmail.com>
2023-07-15 16:37:32 -0500

    hooks

---
+++ src/test/java/cloud/sprint2/step_definitions/Hooks.java
@@ -0,0 +1,27 @@
+package cloud.sprint2.step_definitions;
+
+import cloud.sprint2.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+
+
+    @After
+    public void tearDown(Scenario scenario){
+
+        if (scenario.isFailed()){
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+        System.out.println("This is how @After annotation is working, after each scenario");
+        Driver.closeDriver();
+
+
+
+    }
+}
