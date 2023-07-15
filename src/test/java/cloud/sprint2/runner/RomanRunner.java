120ed9bf52fde5b4c2a99e063ee401b895d06d6d
Roman <romanaliev808@gmail.com>
2023-07-15 16:26:54 -0500

    config

---
+++ src/test/java/cloud/sprint2/runner/RomanRunner.java
@@ -0,0 +1,23 @@
+package cloud.sprint2.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "cloud/sprint2/step_definitions",
+        publish = true,
+        dryRun = false,
+        tags = ""
+
+)
+
+public class RomanRunner { }
