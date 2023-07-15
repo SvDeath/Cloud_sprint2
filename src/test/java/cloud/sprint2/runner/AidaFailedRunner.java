fe310c81a5149782f6d1298e289132f3f7f1d556
Roman <romanaliev808@gmail.com>
2023-07-15 17:21:11 -0500

    failedRunner

---
+++ src/test/java/cloud/sprint2/runner/AidaFailedRunner.java
@@ -0,0 +1,17 @@
+package cloud.sprint2.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+
+            features = "@target/rerun.txt",
+            glue = "cloud/sprint2/step_definitions"
+    )
+
+    public class AidaFailedRunner { }
